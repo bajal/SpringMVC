81740205b41b236e7e76e32b998b244f73103985
Mohiyudeen Bajal <mohiyudeen.bajal@nytimes.com>
2016-04-22 16:53:59 -0400

    init repo

---
+++ src/main/java/com/mohbajal/learnmvc/HelloController.java
@@ -0,0 +1,20 @@
+package com.mohbajal.learnmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.ModelMap;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController{
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String printHello(ModelMap model) {
+        System.out.println("hi");
+        model.addAttribute("message", "Hello Spring MVC Framework!");
+
+        return "hello";
+    }
+
+}
